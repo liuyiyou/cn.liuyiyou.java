1388c3bd9d4287989f03d252358c8eaeff91fce6
liuyiyou <liuyiyou@ibalife.com>
2019-05-06 14:41:48 +0800

    测试

--- src/main/java/cn/liuyiyou/java/collection/list/ArrayListExample.java
+++ src/main/java/cn/liuyiyou/java/collection/list/ArrayListExample.java
@@ -3,6 +3,8 @@ package cn.liuyiyou.java.collection.list;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author: liuyiyou.cn
@@ -23,5 +25,26 @@ public class ArrayListExample {
         strings.add("you");
         ArrayList<String> list = (ArrayList<String>) strings.subList(0, 2);
         System.out.println(list.size());
+    }
+
+
+    @Test
+    public void remove(){
+        List<String> list = getList();
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            System.out.println(next);
+            iterator.remove();
+        }
+    }
+
+
+    public List<String>  getList(){
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("liu");
+        strings.add("yi");
+        strings.add("you");
+        return strings;
     }
 }
