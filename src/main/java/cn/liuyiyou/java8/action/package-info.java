670b1d66677bac5088383f2e4a771c3339ffff7a
liuyiyou <liuyiyou@ibalife.com>
2019-05-07 11:38:27 +0800

    一些测试

---
+++ src/main/java/cn/liuyiyou/java8/action/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * Java8实战
+ * @author: liuyiyou.cn
+ * @date: 2019/5/6
+ * @version: V1.0
+ */
+package cn.liuyiyou.java8.action;
--- src/main/java/cn/liuyiyou/java8/date/DateExample1.java
+++ src/main/java/cn/liuyiyou/java8/date/DateExample1.java
@@ -3,12 +3,6 @@ package cn.liuyiyou.java8.date;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 
-/**
- * @author: liuyiyou@yanglaoban.com
- * @date: 2018/9/20
- * @version: V1.0
- * @Copyright: 2018 yanglaoban.com Inc. All rights reserved.
- */
 public class DateExample1 {
 
     public static void main(String[] args) {
